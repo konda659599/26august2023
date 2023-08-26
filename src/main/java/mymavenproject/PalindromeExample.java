ffa6cfa1b40ead89cd5dff741296a62c16116a96
konda659599 <kondareddy0754@gmail.com>
2023-08-26 15:15:36 +0530

    mylatestcommit

---
+++ src/main/java/mymavenproject/PalindromeExample.java
@@ -0,0 +1,22 @@
+package mymavenproject;
+
+public class PalindromeExample {
+	public static void main(String args[]){  
+		  int r,sum=0,temp;    
+		  int n=404;
+		  //It is the number variable to be checked for palindrome  
+		  
+		  temp=n;    
+		  while(n>0){    
+		   r=n%10;  //getting remainder  
+		   sum=(sum*10)+r;    
+		   n=n/10;    
+		  }    
+		  if(temp==sum)    
+		   System.out.println("palindrome number ");    
+		  else    
+		   System.out.println("not palindrome");    
+		}  
+		}  
+
+
